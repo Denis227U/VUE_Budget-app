fc880ac26df41dcdc39b1ba9d676c3efc8495320
Denis Yunyaev <music_is_my_extazy@mail.ru>
2021-03-20 14:30:14 +0300

    add jsconfig.json for Vetur VSCode plugin

--- src/components/BudgetList.vue
+++ src/components/BudgetList.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="budget-list-wrap">
-    <el-card :header='header'>
+    <el-card :header="header">
       <template v-if="!isEmpty">
-        <div class="list-item" v-for='(item, prop) in list' :key='prop'>
+        <div class="list-item" v-for="(item, prop) in list" :key="prop">
           <span class="budget-comment">{{ item.comment }}</span>
           <span class="budget-value">{{ item.value }}</span>
-          <ElButton type='danger' size='mini'>Delete</ElButton>
+          <ElButton type="danger" size="mini">Delete</ElButton>
         </div>
       </template>
-      <el-alert v-else type="info" :title="emptyTitle" :closable='false' />
+      <el-alert v-else type="info" :title="emptyTitle" :closable="false" />
     </el-card>
   </div>
 </template>
@@ -19,36 +19,36 @@ export default {
   props: {
     list: {
       type: Object,
-      default: () => ({})
-    }
+      default: () => ({}),
+    },
   },
   data: () => ({
     header: 'Budget List',
-    emptyTitle: 'Empty List'
+    emptyTitle: 'Empty List',
   }),
   computed: {
     isEmpty() {
       return !Object.keys(this.list).length;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-  .budget-list-wrap {
-    max-width: 500px;
-    margin: auto;
-  }
+.budget-list-wrap {
+  max-width: 500px;
+  margin: auto;
+}
 
-  .list-item {
-    display: flex;
-    align-items: center;
-    padding: 10px 15px;
-  }
+.list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
 
-  .budget-value {
-    font-weight: bold;
-    margin-left: auto;
-    margin-right: 20px;
-  }
+.budget-value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
 </style>
